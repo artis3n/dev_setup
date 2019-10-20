d0b9fddbf86cdacfbf913c68c3ec0ff908222eeb
Artis3n <dev@quantummadness.com>
2019-10-20 14:23:04 -0400

    Fixed typo in git tasks. Added metadata to github_version plugin

--- lookup_plugins/github_version.py
+++ lookup_plugins/github_version.py
@@ -1,30 +1,59 @@
+# (c) 2019, Ari Kalfus <dev@quantummadness.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 # python 3 headers, required if submitting to Ansible
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-DOCUMENTATION = r"""
+DOCUMENTATION = """
 lookup: github_version
 author:
-  - Artis3n <artis3n@quantummadness.com>
+  - Ari Kalfus (@Artis3n) <dev@quantummadness.com>
 version_added: "2.8"
 requirements:
   - requests
-short_description: Get latest release version from a public Github repository
+short_description: Get the latest tagged release version from a public Github repository.
 description:
   - This lookup returns the latest release tag of a public Github repository.
+  - A future implementation will accept an optional Github token to allow lookup of private repositories.
 options:
-  _repos:
-    description: Github repositories from which to get versions
+  repos:
+    description: A list of Github repositories from which to retrieve versions.
     required: True
 notes:
-  - The version tag is returned however it is defined by the Github repository.
+  - The version tag is returned however it is defined by the Github repository. Most repositories used the convention 'vX.X.X' for a tag, while some use 'X.X.X'. Some may use release tagging structures other than semver. This plugin does not perform opinionated formatting of the release tag structure. Users should format the value via filters after calling this plugin, if needed.
+"""
+
+EXAMPLES = """
+- name: Strip the 'v' out of the tag version, e.g. 'v1.0.0' -> '1.0.0'
+  set_fact:
+    nvm_version: "{{ lookup('github_version', 'nvm-sh/nvm')[1:] }}"
+
+- name: Operate on multiple repositories
+  git:
+    repo: https://github.com/{{ item }}.git
+    version: "{{ lookup('github_version', item) }}"
+    dest: "{{ lookup('env', 'HOME') }}/projects"
+  with_items:
+    - ansible/ansible
+    - ansible/molecule
+    - ansible/awx
+"""
+
+RETURN = """
+  _list:
+    description:
+      - List of latest Github repository version(s)
+    type: list
 """
 
 from ansible.errors import AnsibleError, AnsibleParserError
 from ansible.plugins.lookup import LookupBase
 from ansible.utils.display import Display
-import requests
 from ansible.module_utils._text import to_native
+
+from requests import get
+from requests.exceptions import RequestException
 
 display = Display()
 
@@ -47,7 +76,7 @@ class LookupModule(LookupBase):
 
             # Retrieve the Github API Releases JSON
             try:
-                github_request = requests.get('https://api.github.com/repos/%s/releases/latest' % repo,
+                github_request = get('https://api.github.com/repos/%s/releases/latest' % repo,
                 headers={ 'Accept': 'application/vnd.github.v3+json' }
                 )
                 content = github_request.json()
@@ -55,8 +84,9 @@ class LookupModule(LookupBase):
                 if version != None and len(version) != 0:
                     versions.append(version)
                 else:
-                    raise AnsibleError("Error extracting version from Github API response: %s" % github_request.text)
-            except requests.exceptions.RequestException as e:
+                    raise AnsibleError("Error extracting version from Github API response:\n%s" % github_request.text)
+            # Any errors raised by the requests library inherit from RequestException
+            except RequestException as e:
                 raise AnsibleError("Error communicating with the Github API: %s" % to_native(e))
 
             display.vvvv(u"Github version lookup using %s as repo" % repo)

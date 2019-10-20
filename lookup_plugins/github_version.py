0ae6336f3754e9f0af8f2f296cc80000248a3bca
Artis3n <dev@quantummadness.com>
2019-10-19 22:04:42 -0400

    Working on adding a custom lookup plugin

---
+++ lookup_plugins/github_version.py
@@ -0,0 +1,60 @@
+# python 3 headers, required if submitting to Ansible
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = """
+      lookup: github_version
+        author: Artis3n <artis3n@quantummadness.com>
+        version_added: "1.0"
+        short_description: Get latest release version from a public Github repository
+        description:
+            - This lookup returns the latest release tag of a public Github repository.
+        options:
+          _terms:
+            description: Github repositories from which to get versions
+            required: True
+        notes:
+          - The version tag is returned however it is defined by the Github repository. Usually projects tag either as "1.0.0" or "v1.0.0" but this plugin does not do any formatting after retrieval of the tag.
+"""
+from ansible.errors import AnsibleError, AnsibleParserError
+from ansible.plugins.lookup import LookupBase
+from ansible.utils.display import Display
+from requests import HttpSession
+from ansible.module_utils._text import to_native
+
+display = Display()
+
+
+class LookupModule(LookupBase):
+
+    def run(self, repos, variables=None, **kwargs):
+        # lookups in general are expected to both take a list as input and output a list
+        # this is done so they work with the looping construct 'with_'.
+        versions = []
+        try:
+            for repo in repos:
+
+                # TODO: Sanitize and validate repo
+                if not repo:
+                    # The Parser error indicates invalid options passed
+                    raise AnsibleParserError()
+
+                display.debug("Github version lookup term: %s" % repo)
+
+                # Retrieve the Github API Releases JSON
+                try:
+                    github_request = requests.get('https://api.github.com/repos/%s/releases/latest' % repo,
+                    headers={ 'Accept': 'application/vnd.github.v3+json' }
+                    )
+                    content = github_request.json()
+                    version = content[0].tag_name
+                    if version != None and len(version) != 0:
+                        versions.append(version)
+                    else:
+                        raise AnsibleError("Error extracting version from Github API response: %s" % github_request.text)
+                except requests.exceptions.RequestException as e:
+                    raise AnsibleError("Error communicating with the Github API: %s" % to_native(e))
+
+                display.vvvv(u"Github version lookup using %s as repo" % repo)
+
+        return versions
